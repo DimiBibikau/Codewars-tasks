7e4b9018eb97eb9c484317ad3a55b174f0145fde
Dimi Bibikau <dmitry.bibikov@gmail.com>
2022-12-17 23:06:23 +0100

    fix: solution works on codewars, but not on my mac

--- RSS-Front-End-Q3/leetcode/get-set-codewars.js
+++ RSS-Front-End-Q3/leetcode/get-set-codewars.js
@@ -1,10 +1,8 @@
 class Cube {
     constructor(length){
         this._length = length;
-        this._volume = length ** 3;
-        // this._volume = volume;
-        // this._length = Math.cbrt(this._volume);
-        // this._surfaceArea = (this.length ** 2) * 6;
+        this._volume = this._length ** 3;
+        this._surfaceArea = (this._length ** 2) * 6;
     }
 
     get length(){
@@ -15,39 +13,37 @@ class Cube {
         return this._volume;
     }
 
+    get surfaceArea(){
+        return this._surfaceArea;
+    }
+
     set length(newLength) {
         this._length = newLength;
-        this._volume = newLength ** 3;
+        this._volume = this._length ** 3;
+        this._surfaceArea = (this._length ** 2) * 6;
     }
 
     set volume(newVolume) {
-        this._volume = volume;
-        this._length = Math.cbrt(newVolume);
-        // this._surfaceArea = (this._length**2) * 6;
+        this._volume = newVolume;
+        this._surfaceArea = (Math.cbrt(this._volume)) ** 2 * 6;
+        this._length = Math.cbrt(this._volume);
     }
 
-    // get surfaceArea(){
-    //     return this._surfaceArea;
-    // }
-
-    // set surfaceArea(newSurfaceArea) {
-    //     this.surfaceArea = surfaceArea;
-    // }
+    set surfaceArea(newSurfaceArea) {
+        this._surfaceArea = newSurfaceArea;
+        this._volume = (Math.sqrt(this._surfaceArea / 6))**3;
+        this._length = Math.sqrt(this._surfaceArea / 6);
+    }
 }
 
-const cubeTest = new Cube(2)
+const cubeTest = new Cube(1)
 console.log(cubeTest)
-// cubeTest._length = 3
-cubeTest._volume = 27
+cubeTest._length = 3
+cubeTest._volume = 9
+cubeTest._surfaceArea = 54
+
+console.log(cubeTest.length)
+console.log(cubeTest.volume)
+console.log(cubeTest.surfaceArea)
 console.log(cubeTest)
-// console.log(cubeTest._surfaceArea)
 
-// console.log(cubeTest._length)
-// console.log(cubeTest._volume)
-// console.log(cubeTest._surfaceArea)
-// cubeTest._length = 3
-
-
-// console.log(cubeTest.Length)
-// console.log(cubeTest.Volume)
-// console.log(cubeTest.SurfaceArea)
